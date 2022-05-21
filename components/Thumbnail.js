7a12edb694a892770d991260e66db285cf71573a
Iberi Daniel <danieliberi99@gmail.com>
2022-05-21 23:15:47 +0100

    movie details done

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -9,7 +9,7 @@ const Thumbnail = ({ result }) => {
     const baseUrl = 'https://image.tmdb.org/t/p/original/';
 
     return (
-        <div className='group cursor-pointer'>
+        <div className='group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'>
             <Image
                 alt='image'
                 layout='responsive'
@@ -22,7 +22,7 @@ const Thumbnail = ({ result }) => {
             />
             <div className='p-2'>
                 <p className='truncate max-w-md'>{result.overview}</p>
-                <h2 className='mt-1'>{result.title || result.original_name}</h2>
+                <h2 className='mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold'>{result.title || result.original_name}</h2>
                 <p className='flex items-center opacity-0 group-hover:opacity-100'>
                     {result.media_type && `${result.media_type} .`}{" "}
                     {result.release_date || result.first_air_date} .{" "}
